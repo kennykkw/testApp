4088f5070670391d04ed188445db6796032915ef
Kenny Wong <kk.wong@legend-engg.com>
2021-03-22 15:14:57 +0800

    commit #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testapp';
+  values: number[] = [102, 115, 130, 137];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { jqxBarGaugeComponent }  from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxbargauge';  
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    jqxBarGaugeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
